9fa8e514d5735fccd6702322a318847897ad234c
Duy Lam <duylam468213@gmail.com>
2025-06-11 11:02:48 +0700

    add file env

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Tự động load file .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -179,8 +184,10 @@ DATABASES = {
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'AKIASFUIRWB6QXNJLYJ5'
-AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 AWS_STORAGE_BUCKET_NAME = 'my-django-media-bucket-123'
 AWS_S3_REGION_NAME = 'us-west-1'
 
